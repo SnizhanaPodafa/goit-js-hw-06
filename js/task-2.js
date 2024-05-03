057dc534c8d5dc54caa088cb919cd1c910dd8f66
SnizhanaPodafa <156606360+SnizhanaPodafa@users.noreply.github.com>
2024-05-03 18:29:12 +0300

    made HW

--- js/task-2.js
+++ js/task-2.js
@@ -9,6 +9,24 @@
 
 // Візьми код нижче з ініціалізацією екземпляра й викликами методів і встав його після оголошення класу для перевірки коректності роботи. У консоль будуть виведені результати їх роботи. Будь ласка, нічого там не змінюй.
 
+class Storage {
+    #items = []
+    constructor(items) {
+        this.#items = items;
+    }
+
+    getItems() {
+        return this.#items;
+    }
+
+    addItem(newItem) {
+        this.#items.push(newItem);
+    }
+
+    removeItem(itemToRemove) {
+        this.#items = this.#items.filter(item => item !== itemToRemove);
+    }
+}
 
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
